2c8e7f053ce9ff725917552a68eec355a0144d86
Aditya Kumar <96733312+Aditya-21104028@users.noreply.github.com>
2022-02-15 18:42:48 +0530

    Add files via upload

---
+++ ASSIGNMENT 3 OUTPUT.py
@@ -0,0 +1,116 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=== RESTART: /Users/adityakumar/Documents/Python Assgn./ASSIGNMENT 3 CODE.py ===
+ANSWER 1
+
+
+Enter a string: Apple Mango Banana Apple Mango Mango
+{'Apple': 2, 'Mango': 3, 'Banana': 1}
+
+
+ANSWER 2
+
+
+Enter a year (1800-2025): 1999
+Enter a month (1-12): 12
+Enter a date (1-31): 31
+The next date of the given date is:  2000 1 1
+
+
+ANSWER 3
+
+
+Enter length of list: 4
+Enter element of list: 2
+Enter element of list: 3
+Enter element of list: 5
+Enter element of list: 8
+[[(2, 4)], [(3, 9)], [(5, 25)], [(8, 64)]]
+
+
+ANSWER 4
+
+
+Enter Grade Point: 10
+Your Grade is ‘A+’ and 'Outstanding' Performance.
+
+
+ANSWER 5
+
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+      
+       
+        
+         
+          
+
+
+ANSWER 6
+
+
+6(a)
+Enter SID: 21104028
+Enter student name: Aditya Kumar
+Type 'Y' to continue: Y
+Enter SID: 21104072
+Enter student name: Abhishek Nautiyal
+Type 'Y' to continue: Y
+Enter SID: 21104048
+Enter student name: Sparsh Aggarwal
+Type 'Y' to continue: N
+{21104028: 'Aditya Kumar', 21104072: 'Abhishek Nautiyal', 21104048: 'Sparsh Aggarwal'}
+
+
+6(b)
+[(21104072, 'Abhishek Nautiyal'), (21104028, 'Aditya Kumar'), (21104048, 'Sparsh Aggarwal')]
+
+
+6(c)
+[(21104028, 'Aditya Kumar'), (21104048, 'Sparsh Aggarwal'), (21104072, 'Abhishek Nautiyal')]
+
+
+6(d)
+Enter SID: 21104028
+Student with given SID is:  Aditya Kumar
+
+
+ANSWER 7
+
+
+Enter number of terms: 10
+[1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+
+The average of the Fibonacci series is:  14.3
+
+
+ANSWER 8
+
+
+8(a)
+{1, 3, 5, 6, 8}
+
+
+8(b)
+{17, 3, 6, 8, 9, 13}
+
+
+8(c)
+{1, 2, 4, 5}
+
+
+8(d)
+{6, 7, 8, 9, 10}
+
+
+8(e)
+{10, 7}
+
+
